4c3a0451105f145b7194ea50cabf88decbea5479
Kushagra <36281158+kushagra2103@users.noreply.github.com>
2020-08-06 20:39:21 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -93,7 +93,7 @@ class ActionRestaurantSearch(Action):
 				dispatcher.utter_message('No Restaurant found :( Please try again!')
 		else:
 			dispatcher.utter_message('FAILED.')
-		# dispatcher.utter_message(output)
+		
 		return []
 
 
